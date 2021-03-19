a31e36aad5d630acf1b5d4e5e8c617c72eaa6434
Nikolay <n_vinok98@mail.ru>
2021-03-19 14:39:06 +0300

    desctopEnd

--- gulpfile.js
+++ gulpfile.js
@@ -61,8 +61,9 @@ function scripts() {
     return src([
             'node_modules/jquery/dist/jquery.js',
             'app/js/main.js'
+
         ])
-        .pipe(concat('main.min.js'))
+        // .pipe(concat('main.min.js'))
         .pipe(uglify())
         .pipe(dest('app/js'))
         .pipe(browserSync.stream());
