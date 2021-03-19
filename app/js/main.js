a4cb3f66a3be04359c7e70bfb736f3ec32442511
Nikolay <n_vinok98@mail.ru>
2021-03-19 15:37:14 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -8,3 +8,16 @@ $((function () {
     }))
 
 }));
+
+// const anchors = document.querySelectorAll('a[href*="#about"]');
+
+// for (let anchor of anchors) {
+//     anchor.addEventListener("click", function (event) {
+//         event.preventDefault();
+//         const blockID = anchor.getAttribute('href');
+//         document.querySelector('' + blockID).scrollIntoView({
+//             behavior: "smooth",
+//             block: "start"
+//         });
+//     });
+// }
